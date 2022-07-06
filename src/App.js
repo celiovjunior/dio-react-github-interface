b64605e41f1951748bf1ea0b6b80892cb8b85f65
celiovjunior <celio_cjr@hotmail.com>
2022-07-06 04:30:28 -0300

    adding more styles at the homepage component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import Layout from "./components/Layout";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <main>
       <Layout>
-        <div>
-          <img src="https://avatars.githubusercontent.com/u/72048177?v=4" alt="Avatar" />
-          <h1>Celio Jr</h1>
-          <h3>username</h3>
-          <span>celiovjunior</span>
-          <div>
-            <h4>Followers</h4>
-            <span>15</span>
-          </div>
-          <div>
-            <h4>Starreds</h4>
-            <span>15</span>
-          </div>
-          <div>
-            <h4>Following</h4>
-            <span>15</span>
-          </div>
-        </div>
+        <Profile />
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>
---
+++ src/components/Profile/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import * as S from "./styled";
+
+function Profile() {
+  return <S.Wrapper>
+            <S.WrapperImage src="https://avatars.githubusercontent.com/u/72048177?v=4" alt="Avatar" />
+            <S.WrapperInfosUser>
+              <div>
+                <h1>Celio Jr</h1>
+                <S.WrapperUserName>
+                  <h3>username</h3>
+                  <span>celiovjunior</span>
+                </S.WrapperUserName>
+              </div>
+              <S.WrapperStatusCount>
+              <div>
+                <h4>Followers</h4>
+                <span>15</span>
+              </div>
+              <div>
+                <h4>Starreds</h4>
+                <span>15</span>
+              </div>
+              <div>
+                <h4>Following</h4>
+                <span>15</span>
+              </div>
+              </S.WrapperStatusCount>
+            </S.WrapperInfosUser>
+
+  </S.Wrapper>
+};
+
+export default Profile;
